54aae80cc79f67bf35adf7c3e301dc4dfc639942
unknown <genildoalvesn@gmail.com>
2024-07-27 22:37:11 -0300

    atualizacao_home

--- Igreja/urls.py
+++ Igreja/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('parahyba/teste', include('paginas.urls')),
+    path('home/', include('paginas.urls')),
 
 ]
